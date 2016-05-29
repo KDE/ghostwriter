577a23b3324340e8a124c768f19f0f5abeec23ac
wereturtle <wereturtle@users.noreply.github.com>
2016-05-28 22:40:02 -0700

    Bump up copyright year in license header.

--- src/ThemeFactory.cpp
+++ src/ThemeFactory.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2014, 2015 wereturtle
+ * Copyright (C) 2014-2016 wereturtle
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
