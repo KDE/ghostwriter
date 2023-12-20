e9ddc7f8af7b820859ec294b4677d665fb7b2116
Emir SARI <emir_sari@icloud.com>
2023-12-20 21:51:50 +0300

    Add missing argument

--- Messages.sh
+++ Messages.sh
@@ -5,4 +5,4 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Extract for Qt Linguist
-$EXTRACT_TR_STRINGS `find src/ -name \*.cpp -o -name \*.h -name \*.ui` -o $podir/ghostwriter_qt.pot
+$EXTRACT_TR_STRINGS `find src/ -name \*.cpp -o -name \*.h -o -name \*.ui` -o $podir/ghostwriter_qt.pot
